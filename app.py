4063539d5f6ed18022b8727c0313e1b615135320
kavyaramesh18 <122973945+kavyaramesh18@users.noreply.github.com>
2025-02-14 13:46:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ def home():
     return "Hello, Flask with Jenkins!"
 
 if __name__ == '__main__':
+    print("Hello Jenkins", flush=True)  # Ensure this prints in Jenkins
     app.run(host='0.0.0.0', port=5000, debug=True)
